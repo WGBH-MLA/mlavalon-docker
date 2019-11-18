cf8028fc2ced885557798909c43a9e2e322e4814
Jason Corum <jason.corum@gmail.com>
2019-11-18 10:56:17 -0800

    Bringing the env

--- rails_init.sh
+++ rails_init.sh
@@ -13,6 +13,10 @@ chmod 0777 -R /masterfiles
 chown -R app /masterfiles
 cd /home/app/avalon
 
+# Bringing in the envars we need
+# TODO: Something better?
+env | sed 's#^#export #1;s#=#&"#1;s#$#"&#1' > /etc/profile.d/avalon_nginx_env
+
 service nginx restart
 
 su app
